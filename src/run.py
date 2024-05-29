e58f174b86843b4832117f111c379cbcb15d1fb1
CanyonFoot <canyon.foot@twosigma.com>
2024-05-29 13:27:14 -0400

    documentation updates

--- src/run.py
+++ src/run.py
@@ -1,1 +1,9 @@
-from src.01-download import DataDownloaderUploader
+from src.download import DataDownloaderUploader
+from src.process import Processor
+from src.document import DataDocumenter
+from src.export import s3Exporter
+
+DataDownloaderUploader().download_and_upload_main()
+Processor().process_raw_datasets()
+DataDocumenter().document_processed_data()
+s3Exporter().export_all("export_test")
