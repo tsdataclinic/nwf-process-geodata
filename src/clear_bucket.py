249a508c1fb679952d067b72577ed689a34547bd
CanyonFoot <canyon.foot@twosigma.com>
2024-06-05 10:26:29 -0400

    individual delete function

--- src/clear_bucket.py
+++ src/clear_bucket.py
@@ -1,5 +1,5 @@
 from src.common import get_s3_session, BUCKET_NAME
-
+import boto3
 
 def delete_all_objects():
     s3 = get_s3_session().resource("s3")
@@ -8,6 +8,11 @@ def delete_all_objects():
     bucket.objects.all().delete()
     print(f"Successfully deleted all objects in {BUCKET_NAME}")
 
+def delete_key(key_name):
+    s3 = get_s3_session().resource("s3")
+    # Delete the specified key (file) from the S3 bucket
+    s3.Object(BUCKET_NAME, key_name).delete()
+    print(f"Successfully deleted {key_name} from {BUCKET_NAME}")
 
 if __name__ == "__main__":
     delete_all_objects()
